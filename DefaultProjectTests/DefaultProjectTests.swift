831bf11e6215e44d97ab6b54ca423afeb2d47217
garyuu09 <ribereo.minami@gmail.com>
2025-06-04 21:26:20 +0900

    Initial Commit

---
+++ DefaultProjectTests/DefaultProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultProjectTests.swift
+//  DefaultProjectTests
+//
+//  Created by Ryuga on 2025/06/04.
+//
+
+import Testing
+@testable import DefaultProject
+
+struct DefaultProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
